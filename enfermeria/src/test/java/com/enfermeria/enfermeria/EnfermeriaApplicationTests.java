bd6934cf6d7dee0fb1e47957c0235528e9813539
Gerard-B-dev <gerardchank@gmail.com>
2024-09-12 15:04:46 +0200

    first commit

---
+++ enfermeria/src/test/java/com/enfermeria/enfermeria/EnfermeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enfermeria.enfermeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnfermeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
